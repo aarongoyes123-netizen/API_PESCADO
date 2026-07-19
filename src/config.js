16dce856f8397ba76223d47bbb30b50ecbad5147
Aaron Goyes <aarongoyes123@gmail.com>
2026-07-19 18:51:59 -0500

    Configurar puerto y cloudinary

--- src/config.js
+++ src/config.js
@@ -1,10 +1,14 @@
+import dotenv from 'dotenv';
+import cloudinary from 'cloudinary';
 
-config();
+dotenv.config();
 
-cloudinary.config({
+cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-export default cloudinary;
+export const PORT = process.env.PORT || 3000;
+
+export default cloudinary.v2;
